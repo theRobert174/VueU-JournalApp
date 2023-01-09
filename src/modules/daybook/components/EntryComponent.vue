86f4041ec5038b905271bc395baa0be673deec5a
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-01-09 10:44:09 -0600

    Entries

---
+++ src/modules/daybook/components/EntryComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mb-3 pointer p-2 entry-container">
+    <div class="entry-title d-flex">
+        <span class="text-success fs-5 fw-bold">09</span>
+        <span class="mx-1 fs-5">Enero</span>
+        <span class="mx-2 fw-light">2020, lunes</span>
+    </div>
+    <div class="entry-description">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque corrupti unde velit. Ipsa eveniet eligendi ipsam corporis odio maiores consectetur sed in esse aut nam odit unde, libero consequuntur culpa?
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container{
+  border-bottom: 1px solid #2c3e50;
+  transition: 0.2s all ease-in;
+
+  &:hover{
+    background-color: lighten($color: grey, $amount: 45);
+    transition: 0.2s all ease-in;
+  }
+
+  .entry-description{
+    font-size: 12px;
+  }
+}
+</style>
--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,13 +1,32 @@
 <template>
-    <h1>EntryList!!!</h1>
+    <div class="entry-list-container">
+        <div class="px-2 pt-2">
+            <input type="text" name="" id="" class="form-control" placeholder="Buscar Entrada">
+        </div>
+        <div class="entry-scrollarea">
+            <Entry v-for="item in 100" :key="item"/>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
+import { defineAsyncComponent } from 'vue';
 
+export default {
+    components: {
+        Entry: defineAsyncComponent( () => import('@/modules/daybook/components/EntryComponent.vue'))
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.entry-list-container{
+    border-right: 1px solid #2c3e50;
+    height: calc(100vh - 56px);
+}
+.entry-scrollarea{
+    height: calc(100vh - 100px);
+    overflow: scroll;
+    overflow-x: hidden;
+}
 </style>
