62ef7559b286b77c771a55c4c28863cf65f63a43
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-01-09 21:36:12 -0600

    mapGetters

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,20 +1,32 @@
 <template>
     <div class="entry-list-container">
         <div class="px-2 pt-2">
-            <input type="text" name="" id="" class="form-control" placeholder="Buscar Entrada">
+            <input type="text" name="" id="" class="form-control" placeholder="Buscar Entrada" v-model="term">
         </div>
         <div class="entry-scrollarea">
-            <Entry v-for="item in 100" :key="item"/>
+            <Entry v-for="item in entriesByTerm" :key="item"/>
         </div>
     </div>
 </template>
 
 <script>
 import { defineAsyncComponent } from 'vue';
+import { mapGetters } from "vuex";
 
 export default {
     components: {
         Entry: defineAsyncComponent( () => import('@/modules/daybook/components/EntryComponent.vue'))
+    },
+    computed: {
+        ...mapGetters('journal',['getEntriesByTerm']),
+        entriesByTerm(){
+            return this.getEntriesByTerm(this.term)
+        }
+    },
+    data(){
+        return{
+            term: ''
+        }
     }
 }
 </script>
--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -2,8 +2,9 @@
 // export const myGetter = (state) => {
 //     return state.algo
 // }
-export const getEntriesByTerm = (/*state*/) => {
-    //return state.algo
+export const getEntriesByTerm = (state) => (term = '') => {
+    if(term.length === 0) return state.entries
+    return state.entries.filter(entry => entry.text.toLowerCase().includes(term.toLowerCase()))
 }
 export const getEntryById = (/*state*/) => {
     //return state.algo
